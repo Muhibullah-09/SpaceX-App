ffb55fb8e962671504bf8d060bd74c6f1be3c98f
Muhibullah-09 <mc2840696@gmail.com>
2020-11-30 20:52:55 +0500

    Updates

---
+++ precache-manifest.ea18d31a3930159ebc6b074ff8c5a0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ecef99a7526833be15d929675abc19c",
+    "url": "/SpaceX-App/index.html"
+  },
+  {
+    "revision": "7b135acf8b2c749aa25a",
+    "url": "/SpaceX-App/static/css/main.cffc761c.chunk.css"
+  },
+  {
+    "revision": "5b909e66b6544749a46b",
+    "url": "/SpaceX-App/static/js/2.47e58846.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/SpaceX-App/static/js/2.47e58846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b135acf8b2c749aa25a",
+    "url": "/SpaceX-App/static/js/main.ef744073.chunk.js"
+  },
+  {
+    "revision": "4e343a0c12c04161d888",
+    "url": "/SpaceX-App/static/js/runtime-main.f3f65049.js"
+  }
+]);
